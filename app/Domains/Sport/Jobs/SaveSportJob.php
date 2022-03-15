5c60e502569f20f4d2e255764f4bffff19472276
Edgar <romantibuai@gmail.com>
2022-03-15 19:36:37 +0000

    SaveSportJob instance and handle

--- app/Domains/Sport/Jobs/SaveSportJob.php
+++ app/Domains/Sport/Jobs/SaveSportJob.php
@@ -2,18 +2,20 @@
 
 namespace App\Domains\Sport\Jobs;
 
+use App\Data\Models\Sport;
 use Lucid\Units\Job;
 
 class SaveSportJob extends Job
 {
+    private string $name;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($name)
     {
-        //
+        $this->name = $name;
     }
 
     /**
@@ -23,6 +25,10 @@ class SaveSportJob extends Job
      */
     public function handle()
     {
-        //
+        $attributes = [
+            'name' => $this->name
+        ];
+
+        return tap(new Sport($attributes))->save();
     }
 }
