b5f65358599554f0e7c3423b9a3ea9a94b11aca4
Edgar <romantibuai@gmail.com>
2022-03-21 16:25:39 +0000

    served ListSportsFeature in SportsController@index

--- app/Services/Sport/Features/ListSportsFeature.php
+++ app/Services/Sport/Features/ListSportsFeature.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Sport\Features;
 
+use App\Domains\Sport\Jobs\GetSportsJob;
 use Illuminate\Http\Request;
 use Lucid\Units\Feature;
 
@@ -9,6 +10,8 @@ class ListSportsFeature extends Feature
 {
     public function handle(Request $request)
     {
+        $sports = $this->run(GetSportsJob::class);
 
+        return view('sport::welcome', ['data' => $sports]);
     }
 }
--- app/Services/Sport/Http/Controllers/SportController.php
+++ app/Services/Sport/Http/Controllers/SportController.php
@@ -4,14 +4,14 @@ namespace App\Services\Sport\Http\Controllers;
 
 use App\Data\Models\Sport;
 use App\Services\Sport\Features\AddSportFeature;
+use App\Services\Sport\Features\ListSportsFeature;
 use Lucid\Units\Controller;
 
 class SportController extends Controller
 {
     public function index()
     {
-        $sport = Sport::all();
-        return view('sport::welcome', ['data' => $sport]);
+        return $this->serve(ListSportsFeature::class);
     }
 
     public function add()
