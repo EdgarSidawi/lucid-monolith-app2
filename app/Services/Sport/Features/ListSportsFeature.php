2f8f71ac5d4a1c5ad7bc0273f513f40e5a98c216
Edgar <romantibuai@gmail.com>
2022-03-21 16:20:27 +0000

    created listSportsFeature

---
+++ app/Services/Sport/Features/ListSportsFeature.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Sport\Features;
+
+use Illuminate\Http\Request;
+use Lucid\Units\Feature;
+
+class ListSportsFeature extends Feature
+{
+    public function handle(Request $request)
+    {
+
+    }
+}
---
+++ tests/Feature/Services/Sport/ListSportsFeatureTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature\Services\Sport;
+
+use Tests\TestCase;
+use App\Services\Sport\Features\ListSportsFeature;
+
+class ListSportsFeatureTest extends TestCase
+{
+    public function test_list_sports_feature()
+    {
+        $this->markTestIncomplete();
+    }
+}
